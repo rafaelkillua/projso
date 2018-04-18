e15ff9befe52ca331b26c15f5a7f13d9d5d755be
alessandroliafook <alessandro.liafook@gmail.com>
2018-04-18 20:34:35 -0300

    Implementando a execução de multiplas chamadas.

--- lab_syscalls/activity/process.h
+++ lab_syscalls/activity/process.h
@@ -12,18 +12,23 @@ using namespace std;
 
 int process(const vector<Command>& commands) {
 
-    pid_t p, done_p;
-    p = 0;
+    pid_t p = 0;
+    int i;
 
-    p = fork();
+    for(i = 0; i < commands.size(); i++) {
 
-    if(p == 0) {
-        cout << "eu sou filho " << commands[0].args()[0] << endl;
-        execlp(commands[0].filename(), commands[0].filename(), commands[0].argv()[1], NULL);
+        p = fork();
 
-    } else {
+        if(p == 0) {
+            cout << "eu sou filho " << i << commands[i].args()[0] << endl;
+            execvp(commands[i].filename(), commands[i].argv());
+        
+        } 
+    }
+
+    if(p != 0) {
         wait(0);
-        cout << "voltando pro terminal" << endl;
+        cout << "voltando pro terminal after " << i << endl;
     }
 
     return 0;
