dfadf133130c9a7b1ff05393b5c8b9f7720daf79
alessandroliafook <alessandro.liafook@gmail.com>
2018-04-21 16:53:30 -0300

    Simplificando código

--- lab_syscalls/activity/process.h
+++ lab_syscalls/activity/process.h
@@ -10,26 +10,6 @@
 using namespace xeu_utils;
 using namespace std;
 
-// void left_child(int pp[2]) {
-    
-//     close(pp[0]);
-//     dup2(pp[1], 1);
-
-// }
-
-// int right_child(int pp[2]) {
-
-//     //close(pp[1]);
-//     dup2(pp[0], 0);
-//     dup2(pp[1], 1);
-//     return dup(pp[1]);
-// }
-
-void connectProcess(int fd_in, int fd_out) {
-    dup2(fd_in, 0);
-    dup2(fd_out, 1);
-}
-
 int process(const vector<Command>& commands) {
 
     int n = commands.size();
@@ -38,7 +18,8 @@ int process(const vector<Command>& commands) {
     if(n > 1) { for(i = 0; i < n - 1; i ++) {
         pipe(pp[i]);
         cout << pp[i][0] << " " << pp[i][1] << endl;
-    } }    
+        } 
+    }    
 
     for(i = 0; i < n; i++) {
 
@@ -47,17 +28,17 @@ int process(const vector<Command>& commands) {
             if(n > 1) {
                 
                 if(i == 0){ 
-                cout << "primeiro "<< pp[i][1] << endl;
-                connectProcess(0, pp[i][1]); 
-                } 
+                    cout << "primeiro "<< pp[i][1] << endl;
+                    dup2(pp[i][1], 1); 
 
-                else if (i == n - 1) { 
-                cout << "ultimo " << pp[i-1][0] << endl;
-                connectProcess(pp[i-1][0], 1); }
-                
-                else { 
-                cout << "meio " << pp[i-1][0] << " " << pp[i][1] << endl;
-                connectProcess(pp[i-1][0], pp[i][1]); 
+                } else if (i < n - 1) { 
+                    cout << "meio entrada: " << pp[i-1][0] << " saida: " << pp[i][1] << endl;
+                    dup2(pp[i-1][0], 0);
+                    dup2(pp[i][1], 1); 
+
+               } else {
+                    cout << "ultimo " << pp[i-1][0] << endl;
+                    dup2(pp[i-1][0], 0);
                 }                
             }
 
