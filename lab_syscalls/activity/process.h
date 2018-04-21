402633a253f6326f0635c63c93f9f430e76d17fb
alessandroliafook <alessandro.liafook@gmail.com>
2018-04-21 16:29:27 -0300

    Refatorando para funcionar com até 2 comandos

--- lab_syscalls/activity/process.h
+++ lab_syscalls/activity/process.h
@@ -10,54 +10,68 @@
 using namespace xeu_utils;
 using namespace std;
 
-void left_child(int pp[2], int j) {
+// void left_child(int pp[2]) {
     
-    close(pp[0]);
-    dup2(j, 0);
-    dup2(pp[1], 1);
+//     close(pp[0]);
+//     dup2(pp[1], 1);
 
-}
+// }
 
-int right_child(int pp[2]) {
+// int right_child(int pp[2]) {
 
-    //close(pp[1]);
-    dup2(pp[0], 0);
-    dup2(pp[1], 1);
-    return dup(pp[1]);
+//     //close(pp[1]);
+//     dup2(pp[0], 0);
+//     dup2(pp[1], 1);
+//     return dup(pp[1]);
+// }
+
+void connectProcess(int fd_in, int fd_out) {
+    dup2(fd_in, 0);
+    dup2(fd_out, 1);
 }
 
 int process(const vector<Command>& commands) {
 
-    int i, j = 0, pp[2];
     int n = commands.size();
-    
-    for(i = 0; i < n - 1; i = i + 2) {
+    int i, pp[n][2];
 
-        pipe(pp);
-        pp[0] = j;
+    if(n > 1) { for(i = 0; i < n - 1; i ++) {
+        pipe(pp[i]);
+        cout << pp[i][0] << " " << pp[i][1] << endl;
+    } }    
+
+    for(i = 0; i < n; i++) {
 
         if (fork() == 0) {
+        
+            if(n > 1) {
+                
+                if(i == 0){ 
+                cout << "primeiro "<< pp[i][1] << endl;
+                connectProcess(0, pp[i][1]); 
+                } 
 
-            left_child(pp, j);
+                else if (i == n - 1) { 
+                cout << "ultimo " << pp[i-1][0] << endl;
+                connectProcess(pp[i-1][0], 1); }
+                
+                else { 
+                cout << "meio " << pp[i-1][0] << " " << pp[i][1] << endl;
+                connectProcess(pp[i-1][0], pp[i][1]); 
+                }                
+            }
+
             execvp(commands[i].filename(), commands[i].argv());
-
-        }
-
-        if (n - i >= 2 && fork() == 0) {
-
-            j = right_child(pp);
-            cout << "UÉ meio " << j << endl;
-            execvp(commands[i + 1].filename(), commands[i + 1].argv());
-
-        }
-
+        }        
     }
 
-    close(pp[0]);
-    close(pp[1]);
-    cout << "UE final " << j << endl;
-    dup2(1, j);
-    wait(0);
+    if(n > 1){
+        for(i = 0; i < n - 1; i ++) {
+            close(pp[i][0]);
+            close(pp[i][1]);
+        }
+    }
 
+    wait(0);
     return 0;
 }
