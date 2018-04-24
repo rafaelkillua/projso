09ae880d6dd40d2378e6e9f38aaa126b6592a6b4
Matteus Silva <silvamatteus@lsd.ufcg.edu.br>
2018-04-24 09:44:04 -0300

    Fix bug: close fd in dad. Debug mode.
    Result of tutorship: fixed bugs. Debug mode enabled. Pair programming (team).

--- lab_syscalls/activity/process.h
+++ lab_syscalls/activity/process.h
@@ -3,6 +3,8 @@
 #include "unistd.h"
 #include "stdlib.h"
 #include "sys/wait.h"
+#include <sys/types.h>
+#include <sys/stat.h>
 
 #include <iostream>
 #include <vector>
@@ -15,10 +17,12 @@ int process(const vector<Command>& commands) {
     int n = commands.size();
     int i, pp[n][2];
 
-    if(n > 1) { for(i = 0; i < n - 1; i ++) {
+    if(n > 1) { for(i = 0; i < n -1; i ++) {
         pipe(pp[i]);
+#ifdef DEBUG_MODE
         cout << pp[i][0] << " " << pp[i][1] << endl;
-        } 
+#endif
+        }
     }    
 
     for(i = 0; i < n; i++) {
@@ -27,32 +31,43 @@ int process(const vector<Command>& commands) {
         
             if(n > 1) {
                 
-                if(i == 0){ 
+                if(i == 0){
+#ifdef DEBUG_MODE
                     cout << "primeiro "<< pp[i][1] << endl;
-                    dup2(pp[i][1], 1); 
+#endif
+                    close(pp[i][0]);
+                    dup2(pp[i][1], 1);
 
-                } else if (i < n - 1) { 
+                } else if (i < n - 1) {
+#ifdef DEBUG_MODE
                     cout << "meio entrada: " << pp[i-1][0] << " saida: " << pp[i][1] << endl;
+#endif
+                    
+                    close(pp[i-1][1]);
                     dup2(pp[i-1][0], 0);
-                    dup2(pp[i][1], 1); 
+                    
+                    close(pp[i][0]);
+                    dup2(pp[i][1], 1);
 
                } else {
+#ifdef DEBUG_MODE
                     cout << "ultimo " << pp[i-1][0] << endl;
+#endif
+                    close(pp[i-1][1]);
                     dup2(pp[i-1][0], 0);
                 }                
             }
 
             execvp(commands[i].filename(), commands[i].argv());
+        } else {
+            close(pp[i][1]);
+            int statval = 0;
+            pid_t wpid;
+
+            while ((wpid = wait(&statval)) > 0);
+
         }        
     }
 
-    if(n > 1){
-        for(i = 0; i < n - 1; i ++) {
-            close(pp[i][0]);
-            close(pp[i][1]);
-        }
-    }
-
-    wait(0);
     return 0;
 }
