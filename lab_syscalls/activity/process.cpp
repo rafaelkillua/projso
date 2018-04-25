210976ba2b37f00c54ad1b8482f600b577221e33
Rafael Sampaio Tavares <rafael.tavares@ccc.ufcg.edu.br>
2018-04-25 16:22:30 -0300

    Funcionando entrada e saida (feio, mas funciona)

--- lab_syscalls/activity/process.cpp
+++ lab_syscalls/activity/process.cpp
@@ -3,8 +3,9 @@
 #include "unistd.h"
 #include "stdlib.h"
 #include "sys/wait.h"
-#include <sys/types.h>
-#include <sys/stat.h>
+#include "sys/types.h"
+#include "sys/stat.h"
+#include "fcntl.h"
 
 #include <iostream>
 #include <vector>
@@ -18,7 +19,7 @@ int process(const vector<Command>& commands) {
     int i, pp[n][2];
 
     if (n > 1) {
-        for (i = 0; i < n -1; i ++) {
+        for (i = 0; i < n - 1; i ++) {
             pipe(pp[i]);
 #ifdef DEBUG_MODE
             cout << pp[i][0] << " " << pp[i][1] << endl;
@@ -29,13 +30,22 @@ int process(const vector<Command>& commands) {
     for (i = 0; i < n; i++) {
 
         if (fork() == 0) {
-        
+
             if (n > 1) {
                 
                 if (i == 0) {
 #ifdef DEBUG_MODE
                     cout << "primeiro "<< pp[i][1] << endl;
 #endif
+
+                    for (size_t j = 0; j < commands[i].io().size(); j++) {
+                        IOFile io = commands[i].io()[j];
+                        if (io.is_input()) {
+                            int fdin = open(io.path().c_str(), O_RDONLY);
+                            dup2(fdin, 0);
+                            close(fdin);
+                        }
+                    }
                     close(pp[i][0]);
                     dup2(pp[i][1], 1);
 
@@ -54,8 +64,34 @@ int process(const vector<Command>& commands) {
 #ifdef DEBUG_MODE
                     cout << "ultimo " << pp[i-1][0] << endl;
 #endif
+
+                    for (size_t j = 0; j < commands[i].io().size(); j++) {
+                        IOFile io = commands[i].io()[j];
+                        if (io.is_output()) {
+                            int fdout = open(io.path().c_str(), O_WRONLY|O_CREAT, 0666);
+                            dup2(fdout, 1);
+                            close(fdout);
+                        }
+                    }
                     close(pp[i-1][1]);
                     dup2(pp[i-1][0], 0);
+                }
+            }
+
+            for (size_t j = 0; j < commands[i].io().size(); j++) {
+                IOFile io = commands[i].io()[j];
+                if (io.is_input()) {
+                    int fdin = open(io.path().c_str(), O_RDONLY);
+                    dup2(fdin, 0);
+                    close(fdin);
+                }
+            }
+            for (size_t j = 0; j < commands[i].io().size(); j++) {
+                IOFile io = commands[i].io()[j];
+                if (io.is_output()) {
+                    int fdout = open(io.path().c_str(), O_WRONLY|O_CREAT, 0666);
+                    dup2(fdout, 1);
+                    close(fdout);
                 }
             }
 
