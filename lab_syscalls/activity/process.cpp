e97bdb61c8496bb0317f6a73329872a04ceb14c1
alessandroliafook <alessandro.liafook@gmail.com>
2018-04-25 21:17:50 -0300

    Ending io extra

--- lab_syscalls/activity/process.cpp
+++ lab_syscalls/activity/process.cpp
@@ -13,18 +13,48 @@
 using namespace xeu_utils;
 using namespace std;
 
+void conf_file_fd(const Command command){
+
+    for (size_t j = 0; j < command.io().size(); j++) {
+        
+        IOFile io = command.io()[j];
+        int fd;
+
+        if (io.is_input()) {
+        
+            int fdin = open(io.path().c_str(), O_RDONLY);
+            
+            if(io.has_fd())
+                fd = io.fd();
+            else
+                fd = 0;
+
+            dup2(fdin, fd);
+        
+        } else if (io.is_output()) {
+        
+            int fdout = open(io.path().c_str(), O_WRONLY|O_CREAT, 0666);
+
+            if(io.has_fd())
+                fd = io.fd();
+            else
+                fd = 1;
+
+            dup2(fdout, fd);
+        }
+    }
+}
+
 int process(const vector<Command>& commands) {
 
     int n = commands.size();
     int i, pp[n][2];
 
     if (n > 1) {
-        for (i = 0; i < n - 1; i ++) {
+        
+        for (i = 0; i < n - 1; i ++) 
             pipe(pp[i]);
-#ifdef DEBUG_MODE
-            cout << pp[i][0] << " " << pp[i][1] << endl;
-#endif
-        }
+    
     }
 
     for (i = 0; i < n; i++) {
@@ -32,79 +62,29 @@ int process(const vector<Command>& commands) {
         if (fork() == 0) {
 
             if (n > 1) {
-                
-                if (i == 0) {
-#ifdef DEBUG_MODE
-                    cout << "primeiro "<< pp[i][1] << endl;
-#endif
 
-                    for (size_t j = 0; j < commands[i].io().size(); j++) {
-                        IOFile io = commands[i].io()[j];
-                        if (io.is_input()) {
-                            int fdin = open(io.path().c_str(), O_RDONLY);
-                            dup2(fdin, 0);
-                            close(fdin);
-                        }
-                    }
+                if(i < n - 1) {
+
+                    conf_file_fd(commands[i]);
                     close(pp[i][0]);
                     dup2(pp[i][1], 1);
+                }
 
-                } else if (i < n - 1) {
-#ifdef DEBUG_MODE
-                    cout << "meio entrada: " << pp[i-1][0] << " saida: " << pp[i][1] << endl;
-#endif
+                if(i > 0) {
 
+                    conf_file_fd(commands[i]);
                     close(pp[i-1][1]);
-                    dup2(pp[i-1][0], 0);
-
-                    close(pp[i][0]);
-                    dup2(pp[i][1], 1);
-
-               } else {
-#ifdef DEBUG_MODE
-                    cout << "ultimo " << pp[i-1][0] << endl;
-#endif
-
-                    for (size_t j = 0; j < commands[i].io().size(); j++) {
-                        IOFile io = commands[i].io()[j];
-                        if (io.is_output()) {
-                            int fdout = open(io.path().c_str(), O_WRONLY|O_CREAT, 0666);
-                            dup2(fdout, 1);
-                            close(fdout);
-                        }
-                    }
-                    close(pp[i-1][1]);
-                    dup2(pp[i-1][0], 0);
+                    dup2(pp[i-1][0], 0);                    
                 }
             }
 
-            for (size_t j = 0; j < commands[i].io().size(); j++) {
-                IOFile io = commands[i].io()[j];
-                if (io.is_input()) {
-                    int fdin = open(io.path().c_str(), O_RDONLY);
-                    dup2(fdin, 0);
-                    close(fdin);
-                }
-            }
-            for (size_t j = 0; j < commands[i].io().size(); j++) {
-                IOFile io = commands[i].io()[j];
-                if (io.is_output()) {
-                    int fdout = open(io.path().c_str(), O_WRONLY|O_CREAT, 0666);
-                    dup2(fdout, 1);
-                    close(fdout);
-                }
-            }
-
+            conf_file_fd(commands[i]);
             execvp(commands[i].filename(), commands[i].argv());
 
         } else {
 
             close(pp[i][1]);
-            int statval = 0;
-            pid_t wpid;
-
-            while ((wpid = wait(&statval)) > 0);
-
+            wait(0);
         }
     }
 
